ae2946004617bc82fe36362ebb4cdafde050c01f
Saqib <saqib29111@gmail.com>
2020-12-13 01:58:32 +0600

    laravel route middleware

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -5,20 +5,31 @@ use App\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('one', function () {
+//     return "this is one";
+// })->middleware('test');
+
+// Route::get('two', function () {
+//     return "this is two";
+// })->middleware('test');
+
+// Route::get('three', function () {
+//     return "this is three";
+// })->middleware('test');
 
 Route::middleware('test')->group(function(){
 
     Route::get('one', function () {
         return "this is one";
-    })->middleware('test');
+    });
     
     Route::get('two', function () {
         return "this is two";
-    })->middleware('test');
+    });
     
     Route::get('three', function () {
         return "this is three";
-    })->middleware('test');
+    });
     
 });
 
