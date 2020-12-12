bc7f8d5b32263c11d90a75746758cb707c137801
Saqib <saqib29111@gmail.com>
2020-12-13 00:39:32 +0600

    ORM->object rational mapping

--- blog/database/migrations/2020_12_12_174022_create_posts_table.php
+++ blog/database/migrations/2020_12_12_174022_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateCustomersTable extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->integer('votes');
+            $table->string('title');
+            $table->foreignId('user_id');
+            $table->text('description');
+            $table->boolean('status');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CreateCustomersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('posts');
     }
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,15 +1,37 @@
 <?php
 
+use App\Post;
+use App\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('create_customer', function(){
-    DB::table('customers')->insert(
-        ['name' => 'saqib', 'email'=> 'sqib@email', 'votes' => '120']
-    );
+Route::get('create_user', function () {
+    $data = [
+        'title' => 'weolcome',
+        'description' => 'lorem torem porem morem sorem jarem arem',
+        'user_id' => 1,
+        'status' => 0
+    ];   
+
+    Post::create($data);
 });
 
-Route::get('customers', 'CustomerController@index');
+Route::get('add-post', function () {
+    $post = new Post;
+
+    $post->title = 'This is title';
+    $post->description = 'This is descripotion';
+    $post->user_id = 1;
+    $post->status = 1;
+    $post->save();
+});
+
+Route::get('update-post', function () {
+    $post = Post::find(5);
+    $post->title = 'This is new title';
+
+    $post->save();
+});
 
 Route::get('/', function () {
     return view('welcome');
